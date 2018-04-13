2c68bc9bb1972d03d9ec0f1c3b492c9b1270a773
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2018-04-13 14:47:03 +0000

    Adjacency list implementation of Undirected Graph ; added a new method

--- Graph.java
+++ Graph.java
@@ -21,9 +21,13 @@ public class Graph
     public Iterable<Integer> adj(int v)
     { return adj[v]; }
 
+    public int V()
+    { return this.V; }
+
     public static void main(String[] args)
     {
 	int V = StdIn.readInt();
+	int E = StdIn.readInt();
 	Graph G = new Graph(V);
 	
 	while (!StdIn.isEmpty())
