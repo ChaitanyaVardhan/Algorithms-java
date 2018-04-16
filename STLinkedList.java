f04b0e27b75342b746dfe0bbee26df2730e2c273
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2018-04-16 20:58:43 +0000

    linked list implementation of symbol table

--- STLinkedList.java
+++ STLinkedList.java
@@ -1,15 +1,34 @@
-public class ST<Key, Value>
+public class STLinkedList<Key, Value>
 {
-    public ST()
+    private STNode<Key, Value> current;
+    private int n;
+
+    private class STNode<Key, Value>
 	{
+	    Key key;
+	    Value value;
+	    STNode<Key, Value> next;
+	}
+
+    public STLinkedList()
+	{
+	    current = null;
+	    n = 0;
 	}
 
     public void put(Key key, Value value)
     {
+	STNode<Key, Value> oldcurrent = current;
+	current = new STNode<Key, Value>();
+	current.key = key;
+	current.value = value;
+	current.next = oldcurrent;
+	n++;
     }
 
     public Value get(Key key)
     {
+	return null;
     }
 
     public void delete(Key key)
@@ -18,31 +37,36 @@ public class ST<Key, Value>
 
     public boolean contains(Key key)
     {
+	return get(key) != null;
     }
 
     public boolean isEmpty()
     {
+	return current == null;
     }
 
     public int size()
     {
+	return n;
     }
 
-    Public Iterable<Key> keys()
-    {
-    }
+    //    public Iterable<Key> keys()
+    //    {
+    //    }
 
     public static void main(String[] args)
     {
-	ST<String, int> st = new ST<String, int>();
+	STLinkedList<String, Integer> st = new STLinkedList<String, Integer>();
 
-	for (int i = 0; !StdIn.isempty(); i++)
+	for (int i = 0; !StdIn.isEmpty(); i++)
 	    {
 		String key = StdIn.readString();
 		st.put(key, i);
 	    }
 
-	for (String s : st.keys())
-	    StdOut.println(s + " " + st.get(s));
+	StdOut.println("Size of ST is :" + st.size());
+
+	//	for (String s : st.keys())
+	//	    StdOut.println(s + " " + st.get(s));
     }
 }
