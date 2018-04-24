9c70fabc52a2f4c71c4a9d214bc21395db5c751e
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2018-04-24 15:28:28 +0000

    Linked List implementation of Queue

--- Queue.java
+++ Queue.java
@@ -1,23 +1,93 @@
+import java.util.NoSuchElementException;
+import java.util.Iterator;
+
+
 public class Queue<Item> implements Iterable<Item> {
-    private class Node<Item> {
+    private Node first, last;
+    private int n;
+
+    private class Node {
 	Item item;
 	Node next;
 
-	public Node(Item item, Node last) {
+	public Node(Node last, Item item) {
+	    Node oldlast = last;
 	    this.item = item;
-	    Node<Item> oldlast = last;
 	    this.next = null;
-	    oldlast.next = this;
+	    if (isEmpty()) first = last;
+	    else           oldlast.next = this;
 	}
 
     }
 
     public Queue() {
+	first = null;
+	last = null;
 	n = 0;
     }
 
-    public enqueue(Item item) {
-	last = new Node<Item>(item, last);
+    public boolean isEmpty() {
+	return first == null;
     }
-	    
+
+    public int size() {
+	return n;
+    }
+
+    public void enqueue(Item item) {
+	last = new Node(last, item);
+	n++;
+    }
+    
+    public Item dequeue() {
+	if (isEmpty()) throw new NoSuchElementException("Queue is empty");
+	Item item = first.item;
+	first = first.next;
+	n--;
+	if (isEmpty()) last = null;
+	return item;
+    }
+
+    public Iterator<Item> iterator() {
+	return new QueueIterator(first);
+    }
+
+    private class QueueIterator implements Iterator<Item> {
+        Node current;
+
+	public QueueIterator(Node first){
+	    current = first;
+	}
+
+	public boolean hasNext() {
+	    return current != null;
+	}
+
+	public Item next() {
+	    if (!hasNext()) throw new NoSuchElementException();
+	    Item item = current.item;
+	    current = current.next;
+	    return item;
+	}
+
+	public void remove() {throw new UnsupportedOperationException();}
+    }
+
+    public static void main(String[] args) {
+	Queue<String> queue = new Queue<String>();
+	String item;
+
+	while (!StdIn.isEmpty()) {
+	    item = StdIn.readString();
+	    if (!item.equals("-"))
+		queue.enqueue(item);
+	    else if (!queue.isEmpty())
+		StdOut.print(queue.dequeue() + " ");
+	}
+	StdOut.println("(" + queue.size() + " items left on the queue)");
+
+        StdOut.println("Iterator");
+	for (String s : queue)
+	    StdOut.print(s + " ");
+    }
 }
